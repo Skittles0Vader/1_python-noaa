45c08606d8a378406d371d6e46f004d1388f6b78
skittles0Vader <22177307+Skittles0Vader@users.noreply.github.com>
2019-02-19 18:18:01 -0600

    first commit
    copied from some cool dude on stacktrace

---
+++ noaa_api_v2.py
@@ -0,0 +1,73 @@
+# NOAA API V2
+# Documentation can be found at
+# http://www.ncdc.noaa.gov/cdo-web/webservices/v2
+import requests
+
+
+class NOAAData(object):
+    def __init__(self, token):
+        # NOAA API Endpoint
+        self.url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/'
+        self.h = dict(token=token)
+
+    def poll_api(self, req_type, payload):
+        # Initiate http request - kwargs are constructed into a dict and passed as optional parameters
+        # Ex (limit=100, sortorder='desc', startdate='1970-10-03', etc)
+        r = requests.get(self.url + req_type, headers=self.h, params=payload)
+
+        if r.status_code != 200:  # Handle erroneous requests
+            print("Error: " + str(r.status_code))
+        else:
+            r = r.json()
+            try:
+                return r['results']  # Most JSON results are nested under 'results' key
+            except KeyError:
+                return r  # for non-nested results, return the entire JSON string 
+    
+    # Note: **kwargs = *args and **kwargs allow you to pass a variable number of args to a function. (ex. **keyword args)
+    # 1) Fetch available datasets
+    # http://www.ncdc.noaa.gov/cdo-web/webservices/v2#datasets
+    def datasets(self, **kwargs):
+        req_type = 'datasets'
+        return self.poll_api(req_type, kwargs)
+
+    #1a) Fetch data categories
+    # http://www.ncdc.noaa.gov/cdo-web/webservices/v2#dataCategories
+    def data_categories(self, **kwargs):
+        req_type = 'datacategories'
+        return self.poll_api(req_type, kwargs)
+
+    # 1b) Fetch data types
+    # http://www.ncdc.noaa.gov/cdo-web/webservices/v2#dataTypes
+    def data_types(self, **kwargs):
+        req_type = 'datatypes'
+        return self.poll_api(req_type, kwargs)
+
+    # 2a) Fetch available location categories: country, us territory, state, county, or zip code.
+    # http://www.ncdc.noaa.gov/cdo-web/webservices/v2#locationCategories
+    def location_categories(self, **kwargs):
+        req_type = 'locationcategories'
+        return self.poll_api(req_type, kwargs)
+
+    # 2b) Fetch all available locations
+    # http://www.ncdc.noaa.gov/cdo-web/webservices/v2#locations
+    def locations(self, **kwargs):
+        req_type = 'locations'
+        return self.poll_api(req_type, kwargs)
+
+    # 2b1) Fetch All available stations - Stations are where the data comes from (for most datasets) and can be considered the smallest granual of location data. If the desired station is known, all of its data can quickly be viewed
+    # http://www.ncdc.noaa.gov/cdo-web/webservices/v2#stations
+    def stations(self, h, p, **kwargs):
+        req_type = 'stations'
+        return self.poll_api(req_type, kwargs)
+
+    # Fetch information about specific dataset
+    def dataset_spec(self, set_code, **kwargs):
+        req_type = 'datacategories/' + set_code
+        return self.poll_api(req_type, kwargs)
+
+    # Fetch data
+    # http://www.ncdc.noaa.gov/cdo-web/webservices/v2#data
+    def fetch_data(self, **kwargs):
+        req_type = 'data'
+        return self.poll_api(req_type, kwargs)
